2c301dc35dc9f08892c2e5852a7ceb227cb774ad
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-05-27 12:37:48 +0200

    Verify argument types

--- index.js
+++ index.js
@@ -24,27 +24,103 @@ module.exports = class Bitarray {
   }
 
   get (bit) {
+    if (typeof bit !== 'number') {
+      throw new TypeError(`\`bit\` must be a number, received type ${typeof bit} (${bit})`)
+    }
+
     return binding.get(this._handle, bit)
   }
 
   set (bit, value) {
+    if (typeof bit !== 'number') {
+      throw new TypeError(`\`bit\` must be a number, received type ${typeof bit} (${bit})`)
+    }
+
+    if (typeof value !== 'boolean') {
+      throw new TypeError(`\`value\` must be a boolean, received type ${typeof value} (${value})`)
+    }
+
     return binding.set(this._handle, bit, value)
   }
 
   fill (value, start, end) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError(`\`value\` must be a boolean, received type ${typeof value} (${value})`)
+    }
+
+    if (typeof start !== 'number') {
+      throw new TypeError(`\`start\` must be a number, received type ${typeof start} (${start})`)
+    }
+
+    if (typeof end !== 'number') {
+      throw new TypeError(`\`end\` must be a number, received type ${typeof end} (${end})`)
+    }
+
     binding.fill(this._handle, value, start, end)
   }
 
   findFirst (value, pos = 0) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError(`\`value\` must be a boolean, received type ${typeof value} (${value})`)
+    }
+
+    if (typeof pos !== 'number') {
+      throw new TypeError(`\`pos\` must be a number, received type ${typeof pos} (${pos})`)
+    }
+
     return binding.findFirst(this._handle, value, pos)
   }
 
+  firstSet (pos) {
+    return this.findFirst(true, pos)
+  }
+
+  firstUnset (pos) {
+    return this.findFirst(false, pos)
+  }
+
   findLast (value, pos = -1) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError(`\`value\` must be a boolean, received type ${typeof value} (${value})`)
+    }
+
+    if (typeof pos !== 'number') {
+      throw new TypeError(`\`pos\` must be a number, received type ${typeof pos} (${pos})`)
+    }
+
     return binding.findLast(this._handle, value, pos)
   }
 
+  lastSet (pos) {
+    return this.findLast(true, pos)
+  }
+
+  lastUnset (pos) {
+    return this.findLast(false, pos)
+  }
+
   count (value, start, end) {
+    if (typeof value !== 'boolean') {
+      throw new TypeError(`\`value\` must be a boolean, received type ${typeof value} (${value})`)
+    }
+
+    if (typeof start !== 'number') {
+      throw new TypeError(`\`start\` must be a number, received type ${typeof start} (${start})`)
+    }
+
+    if (typeof end !== 'number') {
+      throw new TypeError(`\`end\` must be a number, received type ${typeof end} (${end})`)
+    }
+
     return binding.count(this._handle, value, start, end)
+  }
+
+  countSet (start, end) {
+    return this.count(true, start, end)
+  }
+
+  countUnset (start, end) {
+    return this.count(false, start, end)
   }
 
   destroy () {
